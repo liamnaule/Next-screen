1ce9d4cc2d611017d28052930ec0fe8e808c4320
liamnaule <liamnaule@gmail.com>
2025-08-22 14:48:07 +0300

    updated the index css and tailwind config

--- Frontend/tailwind.config.js
+++ Frontend/tailwind.config.js
@@ -1,9 +1,39 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        muted: {
+          50: '#f8f8f8',
+          100: '#f0f0f0',
+          200: '#e4e4e4',
+          300: '#d1d1d1',
+          400: '#b4b4b4',
+          500: '#9a9a9a',
+          600: '#818181',
+          700: '#6a6a6a',
+          800: '#5a5a5a',
+          900: '#4e4e4e',
+        },
+        primary: {
+          light: '#4f46e5',
+          DEFAULT: '#3730a3',
+          dark: '#312e81',
+        }
+      },
+      lineHeight: {
+        'relaxed': 1.75,
+        'loose': 2,
+      },
+      spacing: {
+        '18': '4.5rem',
+        '88': '22rem',
+      }
+    },
   },
   plugins: [],
 }
-
